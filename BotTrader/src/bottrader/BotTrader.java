c57f8aa5e20a3bfc183c16d5437d1a71ebf63bea
Shengbo Jia <shengbojia@gmail.com>
2019-06-08 13:25:20 -0400

    yot: s

--- BotTrader/src/bottrader/BotTrader.java
+++ BotTrader/src/bottrader/BotTrader.java
@@ -10,6 +10,9 @@ import java.io.PrintWriter;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 class Configuration {
 
@@ -44,8 +47,24 @@ class Configuration {
 
 public class BotTrader {
 
+    BufferedReader from_exchange;
+    PrintWriter to_exchange;
+
+    boolean isTesting = true;
+
+    List<Integer> buyPrices, buySize, sellPrices, sellSize;
+
+    float[] fairValues = new float[7];
+
+    float fairV;
+
+    String symbol;
+
+    private String data;
+
+
     public BotTrader() {
-        initConnection(true);
+        initConnection(isTesting);
     }
 
     public static void main(String[] args) {
@@ -58,8 +77,142 @@ public class BotTrader {
         }
     }
 
-    public void trade() {
+    private void clearData() {
+        buyPrices.clear();
+        buySize.clear();
+        sellPrices.clear();
+        sellSize.clear();
+    }
 
+    private void readData() {
+        int bookIndex = data.indexOf("BOOK");
+
+        if (bookIndex != -1) {
+            int buyIndex = data.indexOf("BUY");
+            int sellIndex = data.indexOf("SELL");
+
+            symbol = data.substring(buyIndex + 5, sellIndex - 1);
+
+            String buyInfo = ;
+
+            if (!(sellIndex - bookIndex < 4)) {
+
+            }
+
+            if (sellIndex + 4 != data.length()) {
+                parseInfo();
+            }
+
+            String sellTemp = "";
+            List<Integer> sellData = new ArrayList<Integer>();
+            if (sellIndex + 4 != data.length()) {
+                String sellInfo = data.substring(sellIndex + 5);
+
+                if (sellInfo.length() > 0) {
+                    for (int i = 0; i < sellInfo.length(); i++) {
+                        if (sellInfo.charAt(i) == ':' || sellInfo.charAt(i) == ' ') {
+                            sellData.add(Integer.parseInt(sellTemp));
+                            sellTemp = "";
+                        } else {
+                            sellTemp += sellInfo.charAt(i);
+                        }
+                    }
+                    sellData.add(Integer.parseInt(sellTemp));
+                    //System.out.println("sellData: " + sellData);
+                    for (int i = 0; i < sellData.size(); i++) {
+                        if (i % 2 == 0) {
+                            sellPrice.add(sellData.get(i));
+                        } else {
+                            sellAmount.add(sellData.get(i));
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+    }
+
+    private void parseInfo(String buyInfo) {
+
+
+        if (!(sellIndex - bookIndex < 4)) {
+            String buyTemp = "";
+
+            List<Integer> buyData = new ArrayList<>();
+
+
+            for (int i = 0; i < buyInfo.length(); i++) {
+                if (buyInfo.charAt(i) == ':' || buyInfo.charAt(i) == ' ') {
+                    buyData.add(Integer.parseInt(buyTemp));
+                    buyTemp = "";
+                } else {
+                    buyTemp += buyInfo.charAt(i);
+                }
+
+            }
+            buyData.add(Integer.parseInt(buyTemp));
+
+            for (int i = 0; i < buyData.size(); i++) {
+                if (i % 2 == 0) {
+                    buyPrices.add(buyData.get(i));
+                } else {
+                    buySize.add(buyData.get(i));
+                }
+            }
+
+        }
+    }
+
+    public void trade() {
+        try {
+            while (true) {
+
+                clearData();
+
+                data = from_exchange.readLine().trim();
+
+                readData();
+
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+            initConnection(isTesting);
+        }
+
+    }
+
+    private float calcFairValue(int type) {
+        if (!buyPrices.isEmpty() && !sellPrices.isEmpty())
+            switch (type) {
+                case 0:
+                    return 1000;
+                case 1: //VALBZ
+                    return simpleFairValue();
+                case 2: // VALE
+                    return simpleFairValue();
+                case 3: //GS
+                    return simpleFairValue();
+                case 4: // MS
+                    return simpleFairValue();
+                case 5:
+                    return simpleFairValue();
+                case 6:
+                    return simpleFairValue();
+                default:
+                    return -1;
+            }
+
+        return -1;
+    }
+
+    private float simpleFairValue() {
+        int maxBid = Collections.max(buyPrices);
+        int minAsk = Collections.min(sellPrices);
+
+        return (float) (maxBid + minAsk) / 2;
     }
 
     private void initConnection(boolean testMode) {
@@ -68,8 +221,8 @@ public class BotTrader {
 
         try {
             Socket skt = new Socket(config.exchange_name(), config.port());
-            BufferedReader from_exchange = new BufferedReader(new InputStreamReader(skt.getInputStream()));
-            PrintWriter to_exchange = new PrintWriter(skt.getOutputStream(), true);
+            from_exchange = new BufferedReader(new InputStreamReader(skt.getInputStream()));
+            to_exchange = new PrintWriter(skt.getOutputStream(), true);
 
             /*
               A common mistake people make is to to_exchange.println() > 1
@@ -85,3 +238,5 @@ public class BotTrader {
         }
     }
 }
+
+
---
+++ upBot.sh
@@ -0,0 +1,2 @@
+scp "C:\Users\Youssef Mohamed\Documents\NetBeansProjects\JaneStreet2019\BotTrader\src\bottrader\BotTrader.java" ubuntu@100.25.118.185:~
+
